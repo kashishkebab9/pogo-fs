c782feeddac3aee7756e2146bdaf9afe7f0fb7b8
Kashish Garg <kashishgarg9@gmail.com>
2024-01-16 20:20:37 -0500

    played with pointcloud stuff, need to make ros node

--- scripts/pogo-front_end.py
+++ scripts/pogo-front_end.py
@@ -6,22 +6,34 @@ import numpy as np
 import math
 import matplotlib
 import matplotlib.pyplot
+from laser_geometry import LaserProjection
 
 bag = bagpy.bagreader('../bags/2023-01-26-15-26-20.bag')
 pose_graph = nx.Graph()
+laser_proj = LaserProjection()
 print(bag.topic_table)
 
 odom_motor_topic = "/scarab41/odom_motor"
-odom_laser_topic = "/scarab41/odom_laser"
+scan_topic = "/scarab41/scan"
 
 odom_motor_csv = bag.message_by_topic(odom_motor_topic)
-odom_laser_csv = bag.message_by_topic(odom_laser_topic)
+scan_csv = bag.message_by_topic(scan_topic)
 
 odom_motor_data = pd.read_csv(odom_motor_csv)
-odom_laser_data = pd.read_csv(odom_laser_csv)
+scan_data = pd.read_csv(scan_csv)
+print(scan_data)
+print(odom_motor_data)
 
-# print(odom_motor_data)
-# print(odom_laser_data)
+
+# for index, row in scan_data.iterrows():
+#     scan_list = []
+#     for i in range(962):
+#         column_string = "ranges_"+str(i)
+#         scan_list.append(row[column_string])
+#     # pointcloud = laser_proj.projectLaser(scan_list)
+
+
+    
 
 prior_pose = None
 current_pose = None
@@ -40,9 +52,9 @@ for index, row in odom_motor_data.iterrows():
         prior_pose = pose
         continue
     current_pose = pose
-    print(pose)
-    print(current_pose)
-    print(prior_pose)
+    # print(pose)
+    # print(current_pose)
+    # print(prior_pose)
 
     if prior_pose != None and current_pose != None:
         # find the l2 norm, determine angle distance
@@ -54,9 +66,9 @@ for index, row in odom_motor_data.iterrows():
         ang_diff = abs(prior_pose[2] - current_pose[2])
 
         if l2_norm > .5 or ang_diff > .5:
-            print(l2_norm)
-            print(ang_diff)
-            print("Adding node and edge")
+            # print(l2_norm)
+            # print(ang_diff)
+            # print("Adding node and edge")
             pose_graph.add_node(node_counter)
             if node_counter > 0:
                 pose_graph.add_edge(node_counter - 1, node_counter)
