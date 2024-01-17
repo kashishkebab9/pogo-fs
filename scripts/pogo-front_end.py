94348509b4631f27dea6ee3594f78b5696e78dd2
Kashish Garg <kashishgarg9@gmail.com>
2024-01-17 11:17:03 -0500

    started pcb capability

--- scripts/pogo-front_end.py
+++ scripts/pogo-front_end.py
@@ -6,34 +6,27 @@ import numpy as np
 import math
 import matplotlib
 import matplotlib.pyplot
-from laser_geometry import LaserProjection
+import os
 
-bag = bagpy.bagreader('../bags/2023-01-26-15-26-20.bag')
 pose_graph = nx.Graph()
-laser_proj = LaserProjection()
+bag = bagpy.bagreader('../bags/2023-01-26-15-26-20.bag')
 print(bag.topic_table)
-
 odom_motor_topic = "/scarab41/odom_motor"
-scan_topic = "/scarab41/scan"
+cloud_topic = "/scarab41/laser_cloud"
 
 odom_motor_csv = bag.message_by_topic(odom_motor_topic)
-scan_csv = bag.message_by_topic(scan_topic)
+cloud_csv = bag.message_by_topic(cloud_topic)
 
 odom_motor_data = pd.read_csv(odom_motor_csv)
-scan_data = pd.read_csv(scan_csv)
-print(scan_data)
-print(odom_motor_data)
+cloud_data = pd.read_csv(cloud_csv)
 
+directory = "../bags/pcd"
+list_of_pcds = None
+for filename in os.listdir(directory):
+    filename=filename[:-4]
+    filename=float(filename)
+    list_of_pcds.append(filename)
 
-# for index, row in scan_data.iterrows():
-#     scan_list = []
-#     for i in range(962):
-#         column_string = "ranges_"+str(i)
-#         scan_list.append(row[column_string])
-#     # pointcloud = laser_proj.projectLaser(scan_list)
-
-
-    
 
 prior_pose = None
 current_pose = None
@@ -52,9 +45,6 @@ for index, row in odom_motor_data.iterrows():
         prior_pose = pose
         continue
     current_pose = pose
-    # print(pose)
-    # print(current_pose)
-    # print(prior_pose)
 
     if prior_pose != None and current_pose != None:
         # find the l2 norm, determine angle distance
@@ -66,9 +56,6 @@ for index, row in odom_motor_data.iterrows():
         ang_diff = abs(prior_pose[2] - current_pose[2])
 
         if l2_norm > .5 or ang_diff > .5:
-            # print(l2_norm)
-            # print(ang_diff)
-            # print("Adding node and edge")
             pose_graph.add_node(node_counter)
             if node_counter > 0:
                 pose_graph.add_edge(node_counter - 1, node_counter)
@@ -80,11 +67,9 @@ print("Generating Graph....")
 fig = matplotlib.pyplot.figure()
 nx.draw(pose_graph, ax=fig.add_subplot())
 if True: 
-    # Save plot to file
     matplotlib.use("Agg") 
     fig.savefig("front-end.png")
 else:
-    # Display interactive viewer
     matplotlib.pyplot.show()
 
 
